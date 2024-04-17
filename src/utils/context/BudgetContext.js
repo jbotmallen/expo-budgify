336a7c1b8a0471ee340ec4709639bde1fe7f4e17
Alex Abainza <abainza.alexandra@gmail.com>
2024-04-17 11:06:51 +0800

    chore: made the amount on the cards  display the income and expenses on the current month

--- src/utils/context/BudgetContext.js
+++ src/utils/context/BudgetContext.js
@@ -5,48 +5,59 @@ import { db } from "../firebase/firebase-config";
 export const BudgetContext = createContext();
 
 export const BudgetProvider = ({ children }) => {
-    const addBudget = async (formData, id) => {
-        try {
-            const userDocRef = doc(db, "budgets", id);
-            
-            const budgetSubcollectionRef = collection(userDocRef, "content");
-            const newBudgetRef = await addDoc(budgetSubcollectionRef, {...formData, createdAt: new Date(), updatedAt: new Date()});
-            
-            return { success: true, data: newBudgetRef.id };
-        } catch (error) {
-            console.error("Error adding document: ", error);
-            return { success: false, msg: error.msg };
-        }
-    };
+  const addBudget = async (formData, id) => {
+    try {
+      const userDocRef = doc(db, "budgets", id);
 
-    const getBudget = async (userId) => {
-        try {
-            // Reference the user's document within the 'budget' collection
-            const userDocRef = doc(db, "budgets", userId);
-            
-            // Reference the 'content' subcollection within the user's document
-            const contentCollectionRef = collection(userDocRef, "content");
-            
-            // Retrieve all documents within the 'content' subcollection
-            const querySnapshot = await getDocs(contentCollectionRef);
-            
-            // Map over the documents to extract their data
-            const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            
-            return data;
-         } catch (error) {
-            console.error("Error getting documents: ", error);
-            return { success: false, msg: error.message };
-         }
+      const budgetSubcollectionRef = collection(userDocRef, "content");
+      const newBudgetRef = await addDoc(budgetSubcollectionRef, {
+        ...formData,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+
+      return { success: true, data: newBudgetRef.id };
+    } catch (error) {
+      console.error("Error adding document: ", error);
+      return { success: false, msg: error.msg };
     }
+  };
 
-    return <BudgetContext.Provider value={{addBudget, getBudget}}>{children}</BudgetContext.Provider>;
+  const getBudget = async (userId) => {
+    try {
+      // Reference the user's document within the 'budget' collection
+      const userDocRef = doc(db, "budgets", userId);
+
+      // Reference the 'content' subcollection within the user's document
+      const contentCollectionRef = collection(userDocRef, "content");
+
+      // Retrieve all documents within the 'content' subcollection
+      const querySnapshot = await getDocs(contentCollectionRef);
+
+      // Map over the documents to extract their data
+      const data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      return data;
+    } catch (error) {
+      console.error("Error getting documents: ", error);
+      return { success: false, msg: error.message };
+    }
+  };
+
+  return (
+    <BudgetContext.Provider value={{ addBudget, getBudget }}>
+      {children}
+    </BudgetContext.Provider>
+  );
 };
 
 export const useBudget = () => {
-    const context = useContext(BudgetContext);
-    if (context === undefined) {
-        throw new Error("useBudget must be used within a BudgetProvider");
-    }
-    return context;
+  const context = useContext(BudgetContext);
+  if (context === undefined) {
+    throw new Error("useBudget must be used within a BudgetProvider");
+  }
+  return context;
 };
