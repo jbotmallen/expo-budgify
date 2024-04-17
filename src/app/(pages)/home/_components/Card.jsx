8a99f1dcec1a56b411a25043fd9ddbe5b624dd7d
Alex Abainza <abainza.alexandra@gmail.com>
2024-04-17 11:26:55 +0800

    chore: made the amount on the cards display the income and expenses on the current month

--- src/app/(pages)/home/_components/Card.jsx
+++ src/app/(pages)/home/_components/Card.jsx
@@ -2,15 +2,21 @@ import { View, Text } from "react-native";
 import React from "react";
 
 export default function Card({ title, data }) {
-  const value = data?.reduce((acc, curr) => Number(acc) + Number(curr.value), 0);
+  const value = data?.reduce(
+    (acc, curr) => Number(acc) + Number(curr.value),
+    0
+  );
 
   return (
     <View className="rounded-xl flex flex-col gap-2">
-      <Text className="text-slate-400 text-lg">{title} FOR
+      <Text className="text-slate-400 text-lg">
+        {title} FOR
         <Text className="text-slate-300 font-bold"> APRIL</Text>
       </Text>
       <View className="w-full h-28 bg-violet-400 rounded-xl py-8 px-5 flex flex-row items-center justify-start">
-        <Text className="text-5xl tracking-widest font-normal">₱ {value.toFixed(2)}</Text>
+        <Text className="text-5xl tracking-widest font-normal">
+          ₱ {value.toFixed(2)}
+        </Text>
       </View>
     </View>
   );
