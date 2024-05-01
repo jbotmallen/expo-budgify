61b41fa71b0b482ed3328fce1fd363384e1a3f48
Alex Abainza <abainza.alexandra@gmail.com>
2024-05-02 00:35:18 +0800

    chore: added search and sort on the records page

--- src/app/(pages)/records/index.tsx
+++ src/app/(pages)/records/index.tsx
@@ -11,6 +11,8 @@ import React, { useEffect, useMemo, useState } from "react";
 import { recordTags } from "@/constants/constants";
 import { useAuth } from "@/utils/context/AuthContext";
 import { useBudget } from "@/utils/context/BudgetContext";
+import { Picker } from "@react-native-picker/picker";
+import { MaterialIcons } from "@expo/vector-icons";
 import {
   convertDateToString,
   numberWithCommas,
@@ -24,6 +26,8 @@ export default function Records() {
   const [editing, setEditing] = useState(false);
   const [editId, setEditId] = useState("");
   const [description, setDescription] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const { user } = useAuth();
   const { getBudget, deleteBudget, editBudget, loading } = useBudget();
@@ -41,8 +45,47 @@ export default function Records() {
     const filtered = records.filter((record) =>
       selected.toLowerCase().includes(record.category.toLowerCase())
     );
+
+    if (searchTerm) {
+      return filtered.filter((record) =>
+        record.description.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
     return filtered;
-  }, [records, selected]);
+  }, [records, selected, searchTerm]);
+
+  const handleSearchTermInput = (text) => {
+    setSearchTerm(text);
+  };
+
+  const sortedRecords = useMemo(() => {
+    const filtered = filteredRecords;
+
+    let sorted = [...filtered];
+    if (sortBy === "AlphabeticalAtoZ") {
+      sorted.sort((a, b) => a.description.localeCompare(b.description));
+    } else if (sortBy === "AlphabeticalZtoA") {
+      sorted.sort((a, b) => b.description.localeCompare(a.description));
+    } else if (sortBy === "AmountAsc") {
+      sorted.sort((a, b) => a.value - b.value);
+    } else if (sortBy === "AmountDesc") {
+      sorted.sort((a, b) => b.value - a.value);
+    } else if (sortBy === "DateAsc") {
+      sorted.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateA.getTime() - dateB.getTime();
+      });
+    } else if (sortBy === "DateDesc") {
+      sorted.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB.getTime() - dateA.getTime();
+      });
+    }
+
+    return sorted;
+  }, [filteredRecords, sortBy]);
 
   const handleDelete = (id: string) => {
     Alert.alert(
@@ -88,7 +131,11 @@ export default function Records() {
 
   const handleEditSubmit = () => {
     try {
-      if(!description) return Alert.alert("Something went wrong!", "Description is required. Please input a description.");
+      if (!description)
+        return Alert.alert(
+          "Something went wrong!",
+          "Description is required. Please input a description."
+        );
 
       const res = editBudget(user.uid, editId, description);
       if (res) {
@@ -102,7 +149,6 @@ export default function Records() {
       );
     }
   };
-
   return (
     <View className="bg-slate-800 h-screen w-screen px-5 flex flex-col gap-5">
       <View className="w-full h-[8%] flex flex-row items-end justify-start gap-2">
@@ -112,6 +158,77 @@ export default function Records() {
         <Text className="text-4xl text-slate-300 font-bold tracking-widest">
           records
         </Text>
+      </View>
+
+      <View
+        className="h-[5vh]  justify-start items-center flex flex-row"
+        style={{ borderColor: "white" }}
+      >
+        <Picker
+          selectedValue={sortBy}
+          style={{
+            height: 50,
+            borderRadius: 20,
+            width: "50%",
+            color: "white",
+            fontSize: 12,
+            backgroundColor: "transparent",
+            borderColor: "white",
+          }}
+          dropdownIconColor="white"
+          onValueChange={(itemValue) => setSortBy(itemValue)}
+          // prompt="Sort by"
+        >
+          <Picker.Item
+            label="Alphabetical (Ascending)"
+            value="AlphabeticalAtoZ"
+            style={{ fontSize: 14 }}
+          />
+          <Picker.Item
+            label="Alphabetical (Descending)"
+            value="AlphabeticalZtoA"
+            style={{ fontSize: 14 }}
+          />
+
+          <Picker.Item
+            label="Amount (Ascending)"
+            value="AmountAsc"
+            style={{ fontSize: 14 }}
+          />
+          <Picker.Item
+            label="Amount (Descending)"
+            value="AmountDesc"
+            style={{ fontSize: 14 }}
+          />
+
+          {/* <Picker.Item label="Date" value="Date" /> */}
+        </Picker>
+        <View
+          style={{
+            flexDirection: "row",
+            flex: 1,
+            alignItems: "center",
+            borderBottomWidth: 1,
+            borderColor: "gray",
+            padding: 0,
+            justifyContent: "space-between",
+          }}
+        >
+          {/* <Text className="text-white">Search</Text> */}
+          <TextInput
+            placeholder="Search"
+            style={{
+              flex: 1,
+              color: "white",
+            }}
+            placeholderTextColor="gray"
+            onChangeText={handleSearchTermInput}
+            value={searchTerm}
+          ></TextInput>
+          <Pressable onPress={() => setSearchTerm("")}>
+            <IonIcons name="close" size={20} color="white" />
+          </Pressable>
+        </View>
       </View>
       {loading ? (
         <ActivityIndicator size={100} color="white" />
@@ -128,12 +245,12 @@ export default function Records() {
             ))}
           </View>
           <ScrollView className="w-full max-h-[65%] pt-0">
-            {filteredRecords.length <= 0 ? (
+            {sortedRecords.length <= 0 ? (
               <Text className="mx-auto text-gray-300 text-5xl">
                 No data found.
               </Text>
             ) : (
-              filteredRecords.map((record) => (
+              sortedRecords.map((record) => (
                 <View
                   className="w-full h-fit max-h-44 border-2 border-slate-400 rounded-3xl flex flex-row justify-between items-center py-4 px-6 mb-3"
                   key={record.id}
@@ -180,7 +297,11 @@ export default function Records() {
                             <IonIcons name="pencil" size={40} color="skyblue" />
                           </Pressable>
                           <Pressable onPress={() => handleDelete(record.id)}>
-                            <IonIcons name="trash" size={40} color="slategray" />
+                            <IonIcons
+                              name="trash"
+                              size={40}
+                              color="slategray"
+                            />
                           </Pressable>
                         </>
                       )}
